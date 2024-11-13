f55be2e4b5935c8822dd412024219be953e98c1c
KiraXG <2629548748@qq.com>
2024-11-13 10:40:34 +0800

    src文件夹别名配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    // src文件夹别名配置
+    alias: {
+      "@": path.resolve("./src")
+    }
+  }
 })
